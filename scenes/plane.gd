6bac66fb2193887fe2975a4de7f9ac737e214abd
Sergei Babrakov <s.a.babrakov@gmail.com>
2024-09-30 09:01:11 +0200

    Collision: hitting the floor

--- scenes/plane.gd
+++ scenes/plane.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
-const GRAVITY: float = 600.0
-const POWER: float = -200.0
+const GRAVITY: float = 1000.0
+const POWER: float = -350.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,7 +14,15 @@ func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
 	fly()
 	move_and_slide()
+	if is_on_floor() == true:
+		die()
 
 func fly() -> void:
 	if Input.is_action_just_pressed("fly") == true:
 		velocity.y = POWER	
+		
+func die() -> void:
+	set_physics_process(false)
+	animated_sprite_2d.stop()
+	
+	
